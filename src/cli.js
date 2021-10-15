3d2398d6602ad820ecc070ece45cbd089a99e08d
Naylya Sunagatova <sunnaylya@github.com>
2021-10-15 19:07:38 +0500

    add to ignore no console and import/extensions rules

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const userName = () => {
-const name  = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + name + '!');
+const userName = () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
+
+export default userName;
